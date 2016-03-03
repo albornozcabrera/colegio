9eacb09c834abbd535f59ee545214fdca0b1a783
Brain Albornoz <albornozcabrera@gmail.com>
2016-03-03 10:58:49 -0500

    new table status

--- database/migrations/2016_03_03_155423_create_status_table.php
+++ database/migrations/2016_03_03_155423_create_status_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProfileTable extends Migration {
+class CreateStatusTable extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -12,10 +12,10 @@ class CreateProfileTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('profiles', function(Blueprint $table)
+		Schema::create('status', function(Blueprint $table)
 		{
-			$table->increments('n_profile_pk');
-			$table->string('c_description');
+			$table->increments('n_status_pk');
+			$table->string('c_name');
 			$table->timestamps();
 		});
 	}
