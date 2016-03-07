e0a7fe113d8e8ff1b760e43469b506088358aa14
Brain Albornoz <albornozcabrera@gmail.com>
2016-03-07 09:17:48 -0500

    estado original

--- database/migrations/2016_03_03_162007_create_users_table.php
+++ database/migrations/2016_03_03_162007_create_users_table.php
@@ -18,8 +18,6 @@ class CreateUsersTable extends Migration {
             $table->string('password', 60);
             $table->rememberToken();
 
-
-
             $table->timestamps();
         });
     }
